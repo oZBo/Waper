f57e74312356fc62f32c7611f6a3cde39d9b0112
Andreychenko <andreychenko.original@gmail.com>
2017-02-10 17:21:57 +0200

    Removed some stuff

--- app/src/main/java/braincollaboration/waper/MainActivity.java
+++ app/src/main/java/braincollaboration/waper/MainActivity.java
@@ -5,9 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 
-import com.jpardogo.android.googleprogressbar.library.FoldingCirclesDrawable;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.NetworkPolicy;
@@ -18,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     private static final String IMAGE_URL = "https://source.unsplash.com/random";
     private ImageView mainContentImageView;
     private FloatingActionButton fab;
-    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private void configureWidgets() {
         mainContentImageView = (ImageView) findViewById(R.id.main_image);
         fab = (FloatingActionButton) findViewById(R.id.fab);
-        mProgressBar = (ProgressBar) findViewById(R.id.google_progress);
-        mProgressBar.setIndeterminateDrawable(new FoldingCirclesDrawable.Builder(this).build());
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mProgressBar.setVisibility(View.VISIBLE);
                 getNewImage();
             }
         });
@@ -47,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         Picasso.with(WaperApp.getCurrentActivity()).load(IMAGE_URL).memoryPolicy(MemoryPolicy.NO_CACHE).networkPolicy(NetworkPolicy.NO_CACHE).into(mainContentImageView, new Callback() {
             @Override
             public void onSuccess() {
-                mProgressBar.setVisibility(View.INVISIBLE);
+
             }
 
             @Override
