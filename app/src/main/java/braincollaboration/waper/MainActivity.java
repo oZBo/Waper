c98159dd7e849ba7e49d310853b0cd9b63be0940
oZBo <andreychenko.original"gmail.com>
2017-02-14 21:14:36 +0200

    Removed Picasso lib.

--- app/src/main/java/braincollaboration/waper/MainActivity.java
+++ app/src/main/java/braincollaboration/waper/MainActivity.java
@@ -6,11 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.MemoryPolicy;
-import com.squareup.picasso.NetworkPolicy;
-import com.squareup.picasso.Picasso;
-
 public class MainActivity extends AppCompatActivity {
 
     private static final String IMAGE_URL = "https://source.unsplash.com/random";
@@ -30,24 +25,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getNewImage();
-            }
-        });
-
-
-    }
-
-    private void getNewImage() {
-        Picasso.with(WaperApp.getCurrentActivity()).load(IMAGE_URL).memoryPolicy(MemoryPolicy.NO_CACHE).networkPolicy(NetworkPolicy.NO_CACHE).into(mainContentImageView, new Callback() {
-            @Override
-            public void onSuccess() {
-
-            }
-
-            @Override
-            public void onError() {
 
             }
         });
+
+
     }
 }
