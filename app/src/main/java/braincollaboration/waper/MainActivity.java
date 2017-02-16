f7fd658d83a20c8939c2f4796132ef467b665827
Vladislav Berdnikov <ennakent@gmail.com>
2017-02-16 22:37:07 +0200

    Tasks completed;  solved case, when stream returns null; device rotation/minimize action solved; changed floatingButton style

--- app/src/main/java/braincollaboration/waper/MainActivity.java
+++ app/src/main/java/braincollaboration/waper/MainActivity.java
@@ -2,11 +2,13 @@ package braincollaboration.waper;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -16,18 +18,33 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    //TODO All comments MUST be in English.
-
+    private static final String IMAGE_LOAD_ERROR_TAG = "Image transfer error";
     private static final String IMAGE_URL = "https://source.unsplash.com/random";
+    private static final String KEY_ONSAVE_ROTATED_IMAGE = "braincollaboration.waper.mainContentImageView";
+    private static final String KEY_ASYNCTASK_RUN_STATE = "braincollaboration.waper.asyncTaskIsRunning";
     private ImageView mainContentImageView;
     private FloatingActionButton fab;
     private ProgressBar progressBar;
+    private Boolean asyncTaskIsRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         configureWidgets();
+
+        if(savedInstanceState != null) {
+            Bitmap bitmap = savedInstanceState.getParcelable(KEY_ONSAVE_ROTATED_IMAGE);
+            if (bitmap != null) { // restores ImageView
+                mainContentImageView.setImageBitmap(bitmap);
+            }
+            asyncTaskIsRunning = savedInstanceState.getBoolean(KEY_ASYNCTASK_RUN_STATE);
+            if (asyncTaskIsRunning) { //if AsyncTask was started and not finished
+                progressBar.setVisibility(ProgressBar.VISIBLE); //progressBar shows.
+                fab.setVisibility(View.INVISIBLE);
+                fab.setEnabled(false); // makes the button unavailable & invisible.
+            }
+        }
     }
 
     private void configureWidgets() {
@@ -46,31 +63,56 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPreExecute() {
-            progressBar.setVisibility(ProgressBar.VISIBLE); //показываем шкалу прогресса
-            fab.setVisibility(View.INVISIBLE); // прячем кнопку
+            progressBar.setVisibility(ProgressBar.VISIBLE); //progressBar shows.
+            fab.setVisibility(View.INVISIBLE);
+            fab.setEnabled(false); // makes the button unavailable & invisible.
+            asyncTaskIsRunning = true;
         }
 
         protected Bitmap doInBackground(Void... params) {
             //TODO check what will happen when you minimize app while asyncTask is working
-            Bitmap mImage = null;
+            // - When the application is minimized or rotated when AsyncTask is working, the AsyncTask code will execute in the background.
+            Bitmap mImage;
             try {
                 InputStream in = new java.net.URL(IMAGE_URL).openStream();
                 mImage = BitmapFactory.decodeStream(in);
             } catch (Exception e) {
-                Log.e("Image transfer error", e.getMessage()); //TODO move LOG_TAG into static final class field
+                Log.e(IMAGE_LOAD_ERROR_TAG, e.getMessage());
+                mImage = null;
             }
-            return mImage; //TODO check what will happen when Stream return null. Maybe use some stub picture in this case?
+
+            return mImage;
+
+            //TODO check what will happen when Stream return null. Maybe use some stub picture in this case?
+            // - In this case will be used code in the 84th line.
         }
 
         protected void onPostExecute(Bitmap result) {
-            //TODO User still can press on this button when it INVISIBLE. Check what will happen when u press button quickly  many times.
-            progressBar.setVisibility(ProgressBar.INVISIBLE); //прячем шкалу прогресса.
+            progressBar.setVisibility(ProgressBar.INVISIBLE); //progressBar hides.
 
-            if (result!=null) mainContentImageView.setImageBitmap(result);//TODO You MUST always use braces in if statements
-            else Toast.makeText(getApplicationContext(), "Ошибка загрузки изображения", Toast.LENGTH_LONG).show(); //TODO move string into res folder.
+            if (result!=null) {
+                mainContentImageView.setImageBitmap(result);
+            } else {
+                Toast imageLoadingErrorToast = Toast.makeText(getApplicationContext(), R.string.image_loading_error, Toast.LENGTH_SHORT);
+                imageLoadingErrorToast.setGravity(Gravity.CENTER, 0, 0);
+                imageLoadingErrorToast.show();
+            }
 
-            fab.setVisibility(View.VISIBLE);  // показываем кнопку
-
+            fab.setVisibility(View.VISIBLE);
+            fab.setEnabled(true);  // makes the button available & visible.
+            asyncTaskIsRunning = false;
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        BitmapDrawable drawable = (BitmapDrawable) mainContentImageView.getDrawable();
+        if (drawable != null) { //saves mainContentImageView if it's not consist in null or only link on empty xml object
+            Bitmap bitmap = drawable.getBitmap();
+            outState.putParcelable(KEY_ONSAVE_ROTATED_IMAGE, bitmap);
+        }
+        outState.putBoolean(KEY_ASYNCTASK_RUN_STATE, asyncTaskIsRunning);
+
+        super.onSaveInstanceState(outState);
     }
 }
